163e175b4c6b4caf94380bc3e654244c8ba21f93
VictorHAS <victorhermesalves@gmail.com>
2019-12-15 00:19:40 -0300

    Add factory, sessions TDD

--- test/functional/session.spec.js
+++ test/functional/session.spec.js
@@ -1,7 +1,7 @@
 const { test, trait } = use('Test/Suite')('Session');
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const User = use('App/Models/User');
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
 
 trait('Test/ApiClient');
 
@@ -9,18 +9,16 @@ test('it should return JWT token when session created', async ({
   assert,
   client
 }) => {
-  await User.create({
-    username: 'Victor Hermes',
+  const sessionPayload = {
     email: 'victor@gmail.com',
     password: '123456'
-  });
+  };
+
+  await Factory.model('App/Models/User').create(sessionPayload);
 
   const response = await client
     .post('/sessions')
-    .send({
-      email: 'victor@gmail.com',
-      password: '123456'
-    })
+    .send(sessionPayload)
     .end();
 
   response.assertStatus(200);
